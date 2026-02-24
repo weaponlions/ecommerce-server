7adabfab16ae0d8537561d36345640f3163c5890
weaponlion2 <weaponlion2@gmail.com>
2026-02-24 11:10:32 +0530

    Update migration

--- Program.cs
+++ Program.cs
@@ -62,6 +62,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // ── Seed database ──
 // using (var scope = app.Services.CreateScope())
 // {
